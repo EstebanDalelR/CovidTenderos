69f24554fecea435b94983b50515b2aebe5eb49b
estebandalelr <estebandalelr@gmail.com>
2020-03-27 21:25:50 -0500

    Added image from drive

--- components/index/StoreCard.tsx
+++ components/index/StoreCard.tsx
@@ -5,17 +5,24 @@ interface Props {
 const StoreCard = ({ title }: Props) => {
   let categories = ["Abarrotes", "Golosinas", "Hielo", "Licores"]
 
-  return (<div className="border border-gray-200 p-4">
-    <h3 className="text-2xl">Super mercado 'El Cedral'</h3>
-    <p className="text-lg text-right">Calle 161 # 8g - 84</p>
-    <ul>
-      {categories.map((category, index) =>
-        <li className="inline mx-1">
-          {category}
-        </li>
-      )}
-    </ul>
-  </div>)
+  return (
+    <div className="border border-gray-200 p-4 flex">
+      <div className="h-20">
+        <img className="h-20" src="https://drive.google.com/uc?id=1H9AveJrJkUjAaC_l1i8nqS-o2Wguwqf0" alt="Foto iman ${title}" />
+      </div>
+      <div>
+        <h3 className="text-2xl">Super mercado 'El Cedral'</h3>
+        <p className="text-lg text-right">Calle 161 # 8g - 84</p>
+        <ul>
+          {categories.map((category, index) =>
+            <li className="inline mx-1" key={category}>
+              {category}
+            </li>
+          )}
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default StoreCard
